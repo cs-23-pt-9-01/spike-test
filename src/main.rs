93d76473db32eb4cfc061e27a029a3b02d60bf69
Jakob <31919330+Jakobzs@users.noreply.github.com>
2023-11-09 13:41:48 +0100

    Add spike test example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // Get current time in milliseconds
+        let now = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_millis();
+
+        // Busy wait for 300 milliseconds
+        while std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_millis()
+            < now + 300
+        {}
+
+        // Sleep for 10 seconds
+        std::thread::sleep(std::time::Duration::from_secs(10));
+    }
 }
